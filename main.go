709a00ff94f992f631dd201772e29383f76620b8
Steven Kaufman <steven.kaufman0813@gmail.com>
2021-02-18 13:22:22 -0600

    added simple server to auth-service

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", os.Getenv("CLIENT_SITE"))
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.Handle("/", http.HandlerFunc(handler))
+
+	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("APP_PORT")), nil)
 }
